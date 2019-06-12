004c240b004967be8bf3faafd9c2a0766ec65eac
madHatter106 <jkomd.283.2112@gmail.com>
2019-06-12 10:58:19 -0400

    corrected logging info

--- Notebooks_n_Scripts/run_bnn.py
+++ Notebooks_n_Scripts/run_bnn.py
@@ -6,6 +6,6 @@ from pymc_models import bayes_nn_model_ARD_1HL_halfCauchy_hyperpriors as bnn
 
 
 if __name__ == "__main__":
-    logger.add("linreg_{time}.log")
+    logger.add("bnn_{time}.log")
     run_model(bnn)
     logger.info("done!")
